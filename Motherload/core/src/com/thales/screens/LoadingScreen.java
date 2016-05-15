409282ad197412c91d0de9a1b85cf9a826ff88a0
Zamerot <JasonSCrowley@hotmail.com>
2016-05-15 12:09:01 +0800

    Updated fonts:

--- Motherload/core/src/com/thales/screens/LoadingScreen.java
+++ Motherload/core/src/com/thales/screens/LoadingScreen.java
@@ -7,17 +7,17 @@ import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.ControllerListener;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.controllers.PovDirection;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.thales.controllers.XboxOnePad;
-
-import sun.java2d.pipe.TextRenderer;
 
 /**
  * Created by choco on 14/05/2016.
@@ -56,7 +56,8 @@ public class LoadingScreen implements Screen, ControllerListener{
 
     private int truckId;
     private String truckIdString;
-    BitmapFont bitmapFont;
+    BitmapFont timerFont;
+    BitmapFont textFont;
 
     double angle = 0;
 
@@ -72,15 +73,15 @@ public class LoadingScreen implements Screen, ControllerListener{
     @Override
     public void show() {
         Controllers.addListener(this);
-
+    createFonts();
         screenWidth = Gdx.graphics.getWidth();
         screenHeight = Gdx.graphics.getHeight();
         excavatorCenterX = screenWidth / 3;
         halfScreenHeight = screenHeight / 2;
 
         int leftSpacer = 10;
-        topRightX = (screenWidth / 3 + leftSpacer) ;
-        topRightY = (screenHeight / 3 + leftSpacer) ;
+        topRightX = excavatorCenterX * 2;
+        topRightY = halfScreenHeight / 2;
 
         centerPointVector = new Vector2(excavatorCenterX, halfScreenHeight);
         optimalPositionVector = new Vector2(excavatorCenterX - 130, halfScreenHeight + 30);
@@ -96,8 +97,18 @@ public class LoadingScreen implements Screen, ControllerListener{
 
         truckId = 42;
         truckIdString = "Truck ID: " + truckId;
-        bitmapFont = new BitmapFont();
-        bitmapFont.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    private void createFonts() {
+        FileHandle fontFile = Gdx.files.internal("arial.ttf");
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontFile);
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 32;
+        textFont = generator.generateFont(parameter);
+        parameter.size = 72;
+        timerFont = generator.generateFont(parameter);
+        generator.dispose();
     }
 
     @Override
@@ -110,11 +121,20 @@ public class LoadingScreen implements Screen, ControllerListener{
         orthographicCamera.update();
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Timer
         spriteBatch.begin();
-
         timeString = calcTime();
-        bitmapFont.draw (spriteBatch, timeString, topRightX, topRightY);
-        bitmapFont.draw (spriteBatch, truckIdString, topRightX, topRightY-20);
+        timerFont.draw (spriteBatch, timeString, 200, 180);
+        spriteBatch.end();
+        // Dividing line
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.line(topRightX, screenHeight-topRightY, screenWidth, screenHeight -topRightY);
+        shapeRenderer.end();
+
+        // Truck ID
+        spriteBatch.begin();
+        textFont.draw (spriteBatch, truckIdString, 185, 90);
         spriteBatch.end();
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
@@ -154,6 +174,11 @@ public class LoadingScreen implements Screen, ControllerListener{
     private String calcTime() {
         float delta = Gdx.graphics.getDeltaTime();
         countDown -= delta;
+
+        if (countDown <= 0)
+        {
+            return "0:00";
+        }
 
         int minutes = (int)countDown / 60;
         int seconds = (int)countDown % 60;
